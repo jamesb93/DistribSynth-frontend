217f34238c09d9dac63a13452595a89199aee7ab
James Bradbury <jamesbradbury93@gmail.com>
2021-01-25 01:55:44 +0000

    use 8080

--- src/components/stores.js
+++ src/components/stores.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client"
 // Development Toggle
-// let socketAddr = "wss://8f43.xyz:4300" // Live
-var socketAddr = "ws://localhost:4300" // Local
+let socketAddr = "wss://8f43.xyz:8080" // Live
+// var socketAddr = "ws://localhost:4300" // Local
 export const socket = io(socketAddr);
 
 socket.on('connect', () => {
