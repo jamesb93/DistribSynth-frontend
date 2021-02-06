93e205f6615239dae7248b17a10bfcbb5991a353
James Bradbury <jamesbradbury93@gmail.com>
2021-02-06 00:12:48 +0000

    render users in the store.js

--- src/components/stores.js
+++ src/components/stores.js
@@ -1,10 +1,17 @@
 import { io } from "socket.io-client"
+import { writable } from 'svelte/store';
+
+export const numUsers = writable(0);
 
 // Development Toggle
-let socketAddr = "wss://8f43.xyz:4300" // Live
-// let socketAddr = "ws://localhost:4300" // Local
+// let socketAddr = "wss://8f43.xyz:4300" // Live
+let socketAddr = "ws://localhost:4300" // Local
 export const socket = io(socketAddr);
 
 socket.on('connect', () => {
     console.log('connected to ', socketAddr)
 })
+
+socket.on('numUsers', (e) => {
+    numUsers.set(e)
+});
