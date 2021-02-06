6cccbee6f905ec0ac878d40c38446a14b28ddc3e
James Bradbury <jamesbradbury93@gmail.com>
2021-02-06 00:12:42 +0000

    control for karplus strong synth

---
+++ src/components/Membrane.svelte
@@ -0,0 +1,11 @@
+<div class="container">
+    <input type="range" min="0" max="100">
+</div>
+
+<style>
+    .container {
+        display: grid;
+        grid-template-columns: auto auto auto auto;
+        width: 50%
+    }
+</style>
