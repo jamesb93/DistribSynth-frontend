5a07dda9ff2e981c5bf3d1050b0888c2ea2ba387
James Bradbury <jamesbradbury93@gmail.com>
2021-02-06 00:12:57 +0000

    move instruments to top level

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,57 @@
 <script>
 	import Grid from "./components/Grid.svelte";
 	import Chat from "./components/Chat.svelte";
-	import Arrow from "./components/Arrow.svelte";
+	import Membrane from "./components/Membrane.svelte"
+    import * as Tone from "tone";
 
-    import { socket } from "./components/stores.js";
-	let numUsers = 0;
+    import { numUsers } from "./components/stores.js";
 
-	socket.on('numUsers', (e) => {
-		numUsers = e
-		console.log('a new user joined')
-	});
+	const synth = new Tone.PluckSynth().toDestination();
+    const sampler = new Tone.Sampler({
+        urls: {
+            A1: "A1.mp3",
+            A2: "A2.mp3",
+        },
+        baseUrl: "https://tonejs.github.io/audio/casio/",
+}   ).toDestination();
+
+    const drums = new Tone.Sampler({
+        urls: {
+            C3: "kick.mp3",
+            D3: "hat.mp3"
+        },
+        baseUrl: "./sounds/"
+    }).toDestination();
+
+
 </script>
 
 <main>
-	{numUsers} are currently connected.
+	{$numUsers} are currently connected.
 	<br>
 	<br>
-	<Grid />
+	<Grid drums={drums} sampler={sampler} synth={synth} />
 	<br>
+	<div class="synth-controls">
+		<Membrane instrument={synth}/>
+	</div>
 	<!-- <Chat /> -->
 
 
 </main>
 
 <style>
+	.synth-controls {
+		display: grid;
+		grid-template-columns: auto auto;
+	}
+
+	.membrane {
+		display: grid;
+		grid-template-columns: auto auto;
+		grid-template-rows: auto auto;
+	}
+
 	main {
 		text-align: center;
 		padding: 1em;
