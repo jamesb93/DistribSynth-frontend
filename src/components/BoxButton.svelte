c0a1e8ee6d98e903a9689c24f83603138396f2fe
James Bradbury <jamesbradbury93@gmail.com>
2021-02-10 01:10:47 +0000

    new button component

---
+++ src/components/BoxButton.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let func = () => {return func};
+    export let text = "";
+</script>
+
+<button on:click={func}>
+    <span>{text}</span>
+</button>
+
+<style>
+    button {
+        border-radius: 3px;
+        background-color: #d3d3d3;
+        border: 1px solid rgba(219, 219, 219, 0.151);
+        height: 40px;
+        width: 80px;
+    }
+
+    button:hover {
+        background-color: #b3b3b3;
+    }
+
+    span {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: grey;
+        font-size: 20px;
+        
+    }
+</style>
