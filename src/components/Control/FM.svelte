ddaf40e7ba37c980326d0a84222eb047bc256615
James Bradbury <jamesbradbury93@gmail.com>
2021-02-10 01:11:02 +0000

    style controls

---
+++ src/components/Control/FM.svelte
@@ -0,0 +1,44 @@
+<script>
+    import Slider from "../Slider.svelte"
+    export let instrument;
+    let harmonicity = 1.0;
+    let frequency = 250;
+    let modulationIndex = 1.0
+
+    const uHarmonicity = () => {
+        instrument.harmonicity.rampTo(harmonicity, 0.1)
+        console.log(instrument.harmonicity.value)
+    };
+    const uFrequency = () => {
+        instrument.frequency.rampTo(frequency, 0.1);
+    };
+    const uModulationIndex = () => {
+        instrument.modulationIndex.rampTo(modulationIndex / 100.0, 0.1);
+    };
+    
+</script>
+
+<div class="container">
+    <span class="title">FM Synthesis</span>
+    <Slider min="40" max="2500" title="Frequency" bind:value={frequency} func={uFrequency} />
+    <Slider min="3" max="15" title="Harmonicity" bind:value={harmonicity} func={uHarmonicity} />
+    <Slider min="1000" max="8000" title="Index" bind:value={modulationIndex} func={uModulationIndex} />
+</div>
+
+<style>
+    .title {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: grey;
+    }
+    .container {
+        background-color: rgba(219, 219, 219, 0.151);
+        display: flex;
+        flex-direction: column;
+        border: 1px solid grey;
+        border-radius: 5px;
+        width: 100%;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        width: 400px;
+    }
+</style>
---
+++ src/components/Control/Kick.svelte
@@ -0,0 +1,54 @@
+<script>
+    import Slider from "../Slider.svelte"
+    export let instrument;
+    let frequency = 50;
+    let attack = 0.1;
+    let decay = 4;
+    let release = 1.4
+    let sustain = 0.01
+
+    const uFrequency = () => {
+        instrument.frequency.rampTo(frequency, 0.1);
+    };
+
+    const uAttack  = () => {
+        instrument.envelope.attack = (attack / 100.0)
+    };
+    const uDecay   = () => {
+        instrument.envelope.decay = (decay / 100.0)
+    };
+    const uRelease = () => {
+        instrument.envelope.release = (release / 100.0)
+    };
+    const uSustain = () => {
+        instrument.envelope.sustain = (sustain / 100.0)
+    };
+</script>
+
+<div class="container">
+    <span class="title">Kick Synth</span>
+    <Slider title="Frequency" min=35 max=90 bind:value={frequency} func={uFrequency} />
+    <Slider title="Octaves" min=2 max=100 bind:value={instrument.octaves} />
+    <Slider title="Attack" min=0 max=100 bind:value={attack} func={uAttack} />
+    <!-- <Slider title="Decay" min=0 max=100 bind:value={decay} func={uDecay} /> -->
+    <!-- <Slider title="Release" min=0 max=400 bind:value={release} func={uRelease} /> -->
+    <Slider title="Sustain" min=0 max=100 bind:value={sustain} func={uSustain} />
+</div>
+
+<style>
+    .title {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: grey;
+    }
+    .container {
+        background-color: rgba(219, 219, 219, 0.151);
+        display: flex;
+        flex-direction: column;
+        border: 1px solid grey;
+        border-radius: 5px;
+        width: 100%;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        width: 400px;
+    }
+</style>
