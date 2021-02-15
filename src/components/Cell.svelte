1eb994a2f6dab63faa1028790a4978cfca14fdc5
James Bradbury <jamesbradbury93@gmail.com>
2021-02-15 18:41:37 +0000

    add a div that overlays each other div for range control

--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -2,26 +2,45 @@
     export let selected;
     export let emph;
     export let toggleFun;
+    export let ranged = 1;
 </script>
 
-<div class="cell-container">
-    <button 
-        class = "base"
-        class:select = {selected}
-        class:emphasis = {emph}
-        on:click = {toggleFun}
-    />
+<div class="meta">
+    <div class="clickable">
+        <button 
+            class = "base"
+            class:select = {selected}
+            class:emphasis = {emph}
+            on:click = {toggleFun}
+        />
+    </div>
+
+    <!-- If the area is in loop range -->
+    {#if ranged}
+        <div class="ranged"></div>
+    {/if}
 </div>
 
 <style>
-    div {
+    /* Div Area Stuff */
+    .meta {
         padding: 5px;
     }
 
+    .clickable {
+        z-index: 99;
+    }
+
+    .ranged {
+        z-index: 1;
+        background-color: rgba(255, 166, 0, 0.5);
+    }
+
+    /* Button Stuff */
     .base {
-        padding: 5px;
         width: 50px;
         height: 50px;
+        padding: 5px;
         border: 3px solid black;
         display: inline-block;
         border-radius: 0px;
