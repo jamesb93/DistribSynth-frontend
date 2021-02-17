708b354b595886379957451919f5086cceb2f000
James Bradbury <jamesbradbury93@gmail.com>
2021-02-17 00:44:55 +0000

    components for building out control interfaces

---
+++ src/components/Control/ControlContainer.svelte
@@ -0,0 +1,17 @@
+
+<div class="container">
+    <slot></slot>
+</div>
+<style>
+    .container {
+        background-color: rgba(219, 219, 219, 0.151);
+        display: flex;
+        flex-direction: column;
+        border: 1px solid grey;
+        border-radius: 5px;
+        width: 100%;
+        padding-top: 10px;
+        padding-bottom: 10px;
+        width: 400px;
+    }
+</style>
---
+++ src/components/Control/ControlTitle.svelte
@@ -0,0 +1,12 @@
+<script type="ts">
+    export let title: string = "placeholder title text"
+</script>
+
+<span class="title">{title}</span>
+
+<style>
+    .title {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: grey;
+    }
+</style>
