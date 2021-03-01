94893487b3c4b37757aa5a1b44baa2ac6a223092
James Bradbury <jamesbradbury93@gmail.com>
2021-03-01 10:58:44 +0000

    waveshaper to snare

--- src/components/Control/Snare.svelte
+++ src/components/Control/Snare.svelte
@@ -5,6 +5,7 @@
     import ControlContainer from "./ControlContainer.svelte";
     import Presets from "./Presets.svelte";
 
+    export let waveshaper;
     export let filter;
     export let envelope;
     export let parameters;
@@ -14,6 +15,7 @@
     $: envelope.decay = parameters.snare.decay
     $: envelope.release = parameters.snare.release
     $: envelope.sustain = parameters.snare.sustain
+    $: waveshaper.order = parameters.snare.order
 
     const uFrequency = () => {
         socket.emit('params::snare', 'frequency', parameters.snare.frequency);
@@ -35,19 +37,25 @@
         socket.emit('params::snare', 'release', parameters.snare.release)
     }
 
+    const uOrder = () => {
+        socket.emit('params::snare', 'order', parameters.snare.order)
+    }
+
     socket.on('params::snare::frequency', (data) => {parameters.snare.frequency = data});
     socket.on('params::snare::attack', (data) => {parameters.snare.attack = data});
     socket.on('params::snare::decay', (data) => {parameters.snare.decay = data});
     socket.on('params::snare::sustain', (data) => {parameters.snare.sustain = data});
     socket.on('params::snare::release', (data) => {parameters.snare.release = data});
+    socket.on('params::snare::order', (data) => {parameters.snare.order = data});
 </script>
 
 <ControlContainer>
     <ControlTitle title="Snare"/>
-    <Slider min="300" max="5000" step="1" title="filter frequency" bind:value={parameters.snare.frequency} func={uFrequency} />
+    <Slider min="300" max="5000" step="1" title="filter freq" bind:value={parameters.snare.frequency} func={uFrequency} />
     <Slider min="0.001" max="1.0" step="0.001" title="attack" bind:value={parameters.snare.attack} func={uAttack} />
     <Slider min="0.001" max="1.0" step="0.001" title="decay" bind:value={parameters.snare.decay} func={uDecay} />
     <Slider min="0.001" max="1.0" step="0.001" title="sustain" bind:value={parameters.snare.sustain} func={uSustain} />
     <Slider min="0.001" max="1.0" step="0.001" title="release" bind:value={parameters.snare.release} func={uRelease} />
+    <Slider min="1" max="50" step="1" title="waveshaping" bind:value={parameters.snare.order} func={uOrder} />
     <Presets bind:data={parameters} key={'snare'} />
 </ControlContainer>
