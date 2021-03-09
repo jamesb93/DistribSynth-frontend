1bbd7f243014f69301c5e2a1ae97e932f93bc2af
James Bradbury <jamesbradbury93@gmail.com>
2021-03-09 11:44:53 +0000

    remove writeable def

--- src/components/stores.js
+++ src/components/stores.js
@@ -1,8 +1,6 @@
 import { io } from "socket.io-client"
 import { writable } from 'svelte/store';
 
-export const numUsers = writable(0);
-
 // Development Toggle
 // let socketAddr = "wss://8f43.xyz:4300" // Live
 let socketAddr = "ws://localhost:4300" // Local
