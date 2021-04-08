865b8852f388cc8a022a957f0d96e8bbd61b85a9
James Bradbury <jamesbradbury93@gmail.com>
2021-04-08 15:14:10 +0100

    updated components for editing parameters

---
+++ src/components/ASlider.svelte
@@ -0,0 +1,87 @@
+<script type="ts">
+    import { recentParamName, recentParamValue } from './stores.js';
+    export let value;
+    export let min: string = "0";
+    export let max: string = "100";
+    export let title: string = "";
+    export let step: string = "any";
+    export let func = () => {};
+    export let showValue = true;
+    $: $recentParamValue = value; 
+    $: value, $recentParamName = title;
+</script>
+
+<div class="pad">
+    <span class="text title">{title}</span>
+    <div class="container">
+        <span class="text min">{min}</span>
+        <input class="slider" type="range" min={min} max={max} step={step} bind:value={value} on:input={func} />
+        <span class="text max">{max}</span>
+    </div>
+    {#if showValue}
+        <input class="textbox" type="text" bind:value={value} />
+    {/if}
+</div>
+
+
+<style>
+
+    .pad {
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .title {
+        text-align: center;
+    }
+    
+    .text {
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        font-size: 10px;
+        color: grey;
+    }
+    .min {
+        text-align: right;
+        padding-right: 2px;
+    }
+    .max {
+        text-align: left;
+        padding-left: 2px;
+    }
+    
+    .slider {
+        -webkit-appearance: none;
+        width: 75%;
+        height: 3px;
+        border-radius: 50%;  
+        background: #d3d3d3;
+        outline: none;
+        opacity: 0.7;
+        -webkit-transition: .2s;
+        transition: opacity .2s;
+    }
+    
+    .slider::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        appearance: none;
+        width: 17px;
+        height: 17px;
+        border-radius: 50%;
+        background: #4c87af;
+        cursor: pointer;
+    }
+    
+    .slider::-moz-range-thumb {
+        width: 17px;
+        height: 17px;
+        border-radius: 50%;
+        background: #4c87af;
+        cursor: pointer;
+    }
+</style>
